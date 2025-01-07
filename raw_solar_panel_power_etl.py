806ac35497fc1f67542cd5bc8ebf5ca5fce60cce
eslamdyab21 <eslamdyab22@gmail.com>
2025-01-07 02:43:33 +0200

    merge instaed of insert

--- raw_solar_panel_power_etl.py
+++ raw_solar_panel_power_etl.py
@@ -13,18 +13,29 @@ def main():
         .getOrCreate()
     )
 
+    source_df = spark.createDataFrame([
+    (1, 'roof panel', 3, 1000, 25),
+    (2, 'pole panel', 5, 1300, 25),
+    (3, 'flush panel', 10, 1500, 25)
+    ], ["id", "name", "capacity_kwh", "intensity_power_rating", "temperature_power_rating"])
+    
+    source_df.createOrReplaceTempView("source_view")
 
     spark.sql("""
-        INSERT INTO SolarX_Raw_Transactions.solar_panel (id, name, capacity_kwh, intensity_power_rating, temperature_power_rating) VALUES
-            (1, 'roof panel', 3, 1000, 25),
-            (2, 'pole panel', 5, 1300, 25),
-            (3, 'flush panel', 10, 1500, 25);
+        MERGE INTO SolarX_Raw_Transactions.solar_panel target
+        USING source_view source
+        ON target.id = source.id
+        WHEN NOT MATCHED THEN 
+            INSERT (id, name, capacity_kwh, intensity_power_rating, temperature_power_rating)
+            VALUES (source.id, source.name, source.capacity_kwh, source.intensity_power_rating, source.temperature_power_rating)
     """)
 
     logging.info(f"""raw-solar-panel-power-etl -> Load solar_panel successfully into iceberg""")
 
 
+    spark.stop()
 
+    
 if __name__ == "__main__":
     logging.basicConfig(level = "INFO")
     main()
