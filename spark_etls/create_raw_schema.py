b0592f2746cf043a07552bf95dedd54bc7c2da77
eslamdyab21 <eslamdyab22@gmail.com>
2025-02-16 06:48:20 +0200

    add raw battery schema

--- spark_etls/create_raw_schema.py
+++ spark_etls/create_raw_schema.py
@@ -16,8 +16,9 @@ def main():
     spark.sql(create_raw_home_power_readings_schema)
     spark.sql(create_raw_solar_panel_schema)
     spark.sql(create_raw_solar_panel_power_readings_schema)
+    spark.sql(create_raw_battery_power_readings_schema)
 
-    logging.info(f"""create-raw-schema-exists -> SolarX_Raw_Transactions tables are created successfully""")
+    logging.info(f"""create-raw-schema -> SolarX_Raw_Transactions tables are created successfully""")
 
 
 if __name__ == "__main__":
