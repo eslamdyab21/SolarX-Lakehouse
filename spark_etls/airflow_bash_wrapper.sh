4800ea34b15d26cb2d04ba38e85f885925456329
eslamdyab21 <eslamdyab22@gmail.com>
2025-02-19 01:28:53 +0200

    handle source_data_type extra paramater

--- spark_etls/airflow_bash_wrapper.sh
+++ spark_etls/airflow_bash_wrapper.sh
@@ -2,5 +2,6 @@ source /home/iceberg/etl_scripts/.env
 
 spark_python_script=$1
 date=$2
+source_data_type=$3
 
-/opt/spark/bin/spark-submit --master spark://spark-master:7077 --num-executors 6 --executor-cores 1 --executor-memory 512M /home/iceberg/etl_scripts/$spark_python_script $date
+/opt/spark/bin/spark-submit --master spark://spark-master:7077 --num-executors 6 --executor-cores 1 --executor-memory 512M /home/iceberg/etl_scripts/$spark_python_script $source_data_type $date
